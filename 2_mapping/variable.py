80c272672cd6497f8eda9e36fc42d13818f829bb
lucieabergeron <64891112+lucieabergeron@users.noreply.github.com>
2020-05-06 13:13:03 +0200

    Update variable.py

--- 2_mapping/variable.py
+++ 2_mapping/variable.py
@@ -1,3 +1,4 @@
-sp="Callithrix_jacchus"
-refGenome="GCF_000004665.1_Callithrix_jacchus-3.2_genomic"
-path="your_path"
+# Enter species name and reference genome name without the ".fa" extension:
+sp="Macaca_mulatta"
+refGenome="rheMac8"
+path="yourpath"
