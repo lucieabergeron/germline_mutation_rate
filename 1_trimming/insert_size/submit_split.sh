3549f1bd32d1f39491f718d30d63d5498b762d64
lucieabergeron <64891112+lucieabergeron@users.noreply.github.com>
2020-05-06 12:23:28 +0200

    Update submit_split.sh

--- 1_trimming/insert_size/submit_split.sh
+++ 1_trimming/insert_size/submit_split.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 ##
-PATH=$path
 mkdir 1.map/submit_split
 COUNT=0
-echo "#!/bin/bash" > $PATH/1.map/mapping_splitted.sh
+echo "#!/bin/bash" > 1.map/mapping_splitted.sh
 while read p; do
         COUNT=$(( $COUNT + 1 ))
-        echo "#!/bin/bash" > $PATH/1.map/submit_split/submit_map_$COUNT
-        echo "#SBATCH --partition normal" >> $PATH/1.map/submit_split/submit_map_$COUNT
-        echo "#SBATCH --mem-per-cpu 5G" >> $PATH/1.map/submit_split/submit_map_$COUNT
-        echo "#SBATCH -c 4" >> $PATH/1.map/submit_split/submit_map_$COUNT
-        echo $p >> $PATH/1.map/submit_split/submit_map_$COUNT;
+        echo "#!/bin/bash" > 1.map/submit_split/submit_map_$COUNT
+        echo "#SBATCH --partition normal" >> 1.map/submit_split/submit_map_$COUNT
+        echo "#SBATCH --mem-per-cpu 5G" >> 1.map/submit_split/submit_map_$COUNT
+        echo "#SBATCH -c 4" >> 1.map/submit_split/submit_map_$COUNT
+        echo $p >> 1.map/submit_split/submit_map_$COUNT;
         ##
-        echo "sbatch -o $PATH/1.map/submit_split/submit_map_$COUNT.out -e $PATH/1.map/submit_split/submit_map_$COUNT.out $PATH/1.map/submit_split/submit_map_$COUNT" >> $PATH/1.map/mapping_splitted.sh
-done < $PATH/1.map/mapping.sh
+        echo "sbatch -o 1.map/submit_split/submit_map_$COUNT.out -e 1.map/submit_split/submit_map_$COUNT.out 1.map/submit_split/submit_map_$COUNT" >> 1.map/mapping_splitted.sh
+done < 1.map/mapping.sh
 echo "Split is done on $COUNT sequences"
 ##
