a970ca43f6834bc2235241b9ae8fa26ff9f40c37
lucieabergeron <64891112+lucieabergeron@users.noreply.github.com>
2020-05-06 11:37:01 +0200

    Update 0_mkdir.sh

--- 0_trimming/0_mkdir.sh
+++ 0_trimming/0_mkdir.sh
@@ -1,37 +1,38 @@
 ## This script creates all the directories needed for the pipeline.
 SPECIES=Macaca_mulatta
+PATH=your_path
 
 ############################################################################################################
-mkdir /home/lucie/MammalianMutation/faststorage/$SPECIES/
+mkdir $PATH/$SPECIES/
 
-mkdir /home/lucie/MammalianMutation/faststorage/$SPECIES/ref_fasta
+mkdir $PATH/$SPECIES/ref_fasta
 
-mkdir /home/lucie/MammalianMutation/faststorage/$SPECIES/fastq_files
-mkdir /home/lucie/MammalianMutation/faststorage/$SPECIES/fastq_files/catenate
+mkdir $PATH/$SPECIES/fastq_files
+mkdir $PATH/$SPECIES/fastq_files/catenate
 
-mkdir /home/lucie/MammalianMutation/faststorage/$SPECIES/trimmed_seq
+mkdir $PATH/$SPECIES/trimmed_seq
 
-mkdir /home/lucie/MammalianMutation/faststorage/$SPECIES/bam_files
-mkdir /home/lucie/MammalianMutation/faststorage/$SPECIES/bam_files/coverage
-mkdir /home/lucie/MammalianMutation/faststorage/$SPECIES/bam_files/coverage/cov.log
-mkdir /home/lucie/MammalianMutation/faststorage/$SPECIES/bam_files/inter_bam
-mkdir /home/lucie/MammalianMutation/faststorage/$SPECIES/bam_files/map.log
-mkdir /home/lucie/MammalianMutation/faststorage/$SPECIES/bam_files/merge.log
-mkdir /home/lucie/MammalianMutation/faststorage/$SPECIES/bam_files/recal.log
-mkdir /home/lucie/MammalianMutation/faststorage/$SPECIES/bam_files/summary
-mkdir /home/lucie/MammalianMutation/faststorage/$SPECIES/bam_files/uniq_rmdup.log
+mkdir $PATH/$SPECIES/bam_files
+mkdir $PATH/$SPECIES/bam_files/coverage
+mkdir $PATH/$SPECIES/bam_files/coverage/cov.log
+mkdir $PATH/$SPECIES/bam_files/inter_bam
+mkdir $PATH/$SPECIES/bam_files/map.log
+mkdir $PATH/$SPECIES/bam_files/merge.log
+mkdir $PATH/$SPECIES/bam_files/recal.log
+mkdir $PATH/$SPECIES/bam_files/summary
+mkdir $PATH/$SPECIES/bam_files/uniq_rmdup.log
 
-mkdir /home/lucie/MammalianMutation/faststorage/$SPECIES/vcf_files
-mkdir /home/lucie/MammalianMutation/faststorage/$SPECIES/vcf_files/inter_vcf
-mkdir /home/lucie/MammalianMutation/faststorage/$SPECIES/vcf_files/back_com.log
-mkdir /home/lucie/MammalianMutation/faststorage/$SPECIES/vcf_files/call.log
-mkdir /home/lucie/MammalianMutation/faststorage/$SPECIES/vcf_files/combine.log
-mkdir /home/lucie/MammalianMutation/faststorage/$SPECIES/vcf_files/gather.log
+mkdir $PATH/$SPECIES/vcf_files
+mkdir $PATH/$SPECIES/vcf_files/inter_vcf
+mkdir $PATH/$SPECIES/vcf_files/back_com.log
+mkdir $PATH/$SPECIES/vcf_files/call.log
+mkdir $PATH/$SPECIES/vcf_files/combine.log
+mkdir $PATH/$SPECIES/vcf_files/gather.log
 
-mkdir /home/lucie/MammalianMutation/faststorage/$SPECIES/vcf_handling
-mkdir /home/lucie/MammalianMutation/faststorage/$SPECIES/vcf_handling/log_file
-mkdir /home/lucie/MammalianMutation/faststorage/$SPECIES/vcf_handling/inter_vcf
+mkdir $PATH/$SPECIES/vcf_handling
+mkdir $PATH/$SPECIES/vcf_handling/log_file
+mkdir $PATH/$SPECIES/vcf_handling/inter_vcf
 
-mkdir /home/lucie/MammalianMutation/faststorage/$SPECIES/de_novo_mutation
-mkdir /home/lucie/MammalianMutation/faststorage/$SPECIES/de_novo_mutation/log_file
-mkdir /home/lucie/MammalianMutation/faststorage/$SPECIES/de_novo_mutation/inter_vcf
+mkdir $PATH/$SPECIES/de_novo_mutation
+mkdir $PATH/$SPECIES/de_novo_mutation/log_file
+mkdir $PATH/$SPECIES/de_novo_mutation/inter_vcf
