493881e81f117b909dad2f223777e7868d18204a
lucieabergeron <64891112+lucieabergeron@users.noreply.github.com>
2020-11-06 15:31:50 +0100

    Update 0.filtering.sh

--- 1_trimming/filtering/0.filtering.sh
+++ 1_trimming/filtering/0.filtering.sh
@@ -9,7 +9,7 @@ PATH=$path
 ##
 ###############################
 ## Construct the file clean.sh file:
-less $PATH/$SP/raw_seq_dir.txt | while read a b c; do echo "SOAPnuke filter -f AAGTCGGAGGCCAAGCGGTCTTAGGAAGACAA -r AAGTCGGATCGTAGCCATGTCGTTCTGTGAGCCAAGGAGTTG -1 $b -2 $c -Q 2 -l 10 -q 0.2 -E 60 -5 0 -M 2 -o $PATH/$SP/trimmed_seq/$(basename $b '_read_1.fq.gz') -C $(basename $b '.fq.gz').clean.fq.gz -D $(basename $c '.fq.gz').clean.fq.gz"; done > clean.sh
+less $PATH/$SP/raw_seq_dir.txt | while read a b c; do echo "SOAPnuke filter -f AAGTCGGAGGCCAAGCGGTCTTAGGAAGACAA -r AAGTCGGATCGTAGCCATGTCGTTCTGTGAGCCAAGGAGTTG -1 $b -2 $c -G -Q 2 -l 10 -q 0.2 -E 60 -5 0 -M 2 -o $PATH/$SP/trimmed_seq/$(basename $b '_read_1.fq.gz') -C $(basename $b '.fq.gz').clean.fq.gz -D $(basename $c '.fq.gz').clean.fq.gz"; done > clean.sh
 ##
 ## Split the files to submit them:
 bash submit_split.sh
